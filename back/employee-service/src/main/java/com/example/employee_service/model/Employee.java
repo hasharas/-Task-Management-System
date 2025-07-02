b95ea5c2fb0a518c053d83c3988df7e76f5b07ca
hasharas <hasharamsankalpam@gmail.com>
2025-07-03 01:56:57 +0530

    Create:employee model create

---
+++ back/employee-service/src/main/java/com/example/employee_service/model/Employee.java
@@ -0,0 +1,29 @@
+package com.example.employee_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+      @Id
+      @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+      private Long id;
+      // i decided thare name and email
+      private String name;
+
+      private String email;
+}
