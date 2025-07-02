082af3e04ae79c510ed4ddff9c454a10004b5288
hasharas <hasharamsankalpam@gmail.com>
2025-07-02 18:17:13 +0530

    Create:password encoder

---
+++ back/auth-service/src/main/java/com/example/auth_service/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+// set up password encoder
+
+package com.example.auth_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+
+      @Bean
+      public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+      }
+}
