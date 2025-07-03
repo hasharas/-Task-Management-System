3ce48963d9586e85ed40ad6776efc9ba6c8c8aab
hasharas <hasharamsankalpam@gmail.com>
2025-07-03 14:05:18 +0530

    Start:service and controller file create

--- back/task-service/src/main/java/com/example/task_service/controller/TaskController.java
+++ back/task-service/src/main/java/com/example/task_service/controller/TaskController.java
@@ -1,5 +1,22 @@
 package com.example.task_service.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.task_service.service.TaskService;
+import com.example.task_service.util.EmployeeValidator;
+
+@RestController
+@RequestMapping("/tasks")
 public class TaskController {
+      private final TaskService taskService;
+      private final EmployeeValidator employeeValidator;
+
+      public TaskController(TaskService taskService, EmployeeValidator employeeValidator) {
+            this.taskService = taskService;
+            this.employeeValidator = employeeValidator;
+      }
+
+      // end pont hare
 
 }
--- back/task-service/src/main/java/com/example/task_service/service/TaskService.java
+++ back/task-service/src/main/java/com/example/task_service/service/TaskService.java
@@ -1,5 +1,13 @@
 package com.example.task_service.service;
 
+import com.example.task_service.repository.TaskRepository;
+
 public class TaskService {
 
+      private final TaskRepository repository;
+
+      public TaskService(TaskRepository repository) {
+            this.repository = repository;
+      }
+      // create logics hare
 }
