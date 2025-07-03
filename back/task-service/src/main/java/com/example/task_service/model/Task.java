dd5e5faee360f2ace2b22fd91ade870aa1a6f39e
hasharas <hasharamsankalpam@gmail.com>
2025-07-03 13:48:49 +0530

    Create:task entity foile

--- back/task-service/src/main/java/com/example/task_service/model/Task.java
+++ back/task-service/src/main/java/com/example/task_service/model/Task.java
@@ -1,5 +1,30 @@
 package com.example.task_service.model;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Task {
 
+      @Id
+      @GeneratedValue(strategy = GenerationType.IDENTITY)
+      private Long id;
+
+      private String title;
+
+      private String description;
+
+      @Enumerated(EnumType.STRING)
+      private Status status;
+
+      private LocalDate dueDate;
+
+      private Long employeeId;
 }
