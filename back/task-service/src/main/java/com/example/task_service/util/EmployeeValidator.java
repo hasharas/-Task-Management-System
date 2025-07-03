6cd2355aa173386c9f32c2665ba3237e065aa345
hasharas <hasharamsankalpam@gmail.com>
2025-07-03 14:12:10 +0530

    Buldfail fix:service file change

--- back/task-service/src/main/java/com/example/task_service/util/EmployeeValidator.java
+++ back/task-service/src/main/java/com/example/task_service/util/EmployeeValidator.java
@@ -1,5 +1,22 @@
 package com.example.task_service.util;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.client.RestTemplate;
+
 public class EmployeeValidator {
 
+      @Value("${employee.service.url}")
+      private String employeeServiceUrl;
+
+      private final RestTemplate restTemplate = new RestTemplate();
+
+      public boolean employeeExists(Long id) {
+            try {
+                  String url = employeeServiceUrl + "/" + id;
+                  restTemplate.getForObject(url, Object.class);
+                  return true;
+            } catch (Exception e) {
+                  return false;
+            }
+      }
 }
