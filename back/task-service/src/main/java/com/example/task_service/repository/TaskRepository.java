c14dff1f0946ae0925258ea042008b5cc85641a6
hasharas <hasharamsankalpam@gmail.com>
2025-07-03 13:56:37 +0530

    Create:create repository file

--- back/task-service/src/main/java/com/example/task_service/repository/TaskRepository.java
+++ back/task-service/src/main/java/com/example/task_service/repository/TaskRepository.java
@@ -1,5 +1,9 @@
 package com.example.task_service.repository;
 
-public class TaskRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.task_service.model.Task;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+      // additinal query methods can be defined here
 
 }
